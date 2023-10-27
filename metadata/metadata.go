8deff97ac577631d54fa91bd48ef60b5f10688e2
Rodrigo Juarez <rodrigo.juarez@wizeline.com>
2023-10-27 08:13:48 -0600

    first commit

---
+++ metadata/metadata.go
@@ -0,0 +1,17 @@
+package metadata
+
+import "fmt"
+
+type ManagerInterface interface {
+	Sample()
+}
+
+type Manager struct{}
+
+func (m *Manager) Sample() {
+	fmt.Println("Good!!")
+}
+
+func GetManager() *Manager {
+	return &Manager{}
+}
